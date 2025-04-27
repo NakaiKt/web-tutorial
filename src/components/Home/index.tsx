0c6cb3c7252470e0c88a66d77db7a224f115118f
KatsuhiroNakai <k.nakai.research@gmail.com>
2025-04-27 14:51:00 +0900

    feat: Homeコンポーネントを作成し、ページコンポーネントで統合

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Typography, Box } from "@mui/material";
+
+const Home = () => {
+  return (
+    <div>
+      <Typography variant="h1">tutorial</Typography>
+      <Typography>
+        これはCSS, MUI, React, ReactHookFormのチュートリアルです
+        <br />
+        環境は以下を参考にしてください
+      </Typography>
+      <Box
+        component="pre"
+        sx={{
+          backgroundColor: "grey.100",
+        }}
+      >
+        <code>
+          Next: 15.3.1
+          <br />
+          React: 19.0.0
+          <br />
+          ReactDOM: 19.0.0
+          <br />
+          ReactHookForm: 7.56.1
+          <br />
+          MUI: 7.0.2
+        </code>
+      </Box>
+    </div>
+  );
+};
+export default Home;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,5 @@
-import { Typography, Box } from "@mui/material";
-
+import Home from "@/components/Home";
 const Pages = () => {
-  return (
-    <div>
-      <Typography variant="h1">tutorial</Typography>
-      <Typography>
-        これはCSS, MUI, React, ReactHookFormのチュートリアルです
-        <br />
-        環境は以下を参考にしてください
-      </Typography>
-      <Box
-        component="pre"
-        sx={{
-          backgroundColor: "grey.100",
-        }}
-      >
-        <code>
-          Next: 15.3.1
-          <br />
-          React: 19.0.0
-          <br />
-          ReactDOM: 19.0.0
-          <br />
-          ReactHookForm: 7.56.1
-          <br />
-          MUI: 7.0.2
-        </code>
-      </Box>
-    </div>
-  );
+  return <Home />;
 };
 export default Pages;
