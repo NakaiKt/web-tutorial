bbfb827678865d6078960dd67a68746156747458
KatsuhiroNakai <k.nakai.research@gmail.com>
2025-04-29 09:55:51 +0900

    feat: MUIテーマにカスタムフォントと新しいTypographyスタイルを追加。codeバリアントを定義し、h1およびh2のスタイルを強化。

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,6 +1,12 @@
 // src/theme/index.tsx などのファイルを作成
 import { createTheme } from "@mui/material/styles";
 
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    code: true;
+  }
+}
+
 const theme = createTheme({
   palette: {
     primary: {
@@ -28,6 +34,41 @@ const theme = createTheme({
     grey: {
       50: "#fafafa",
       100: "#f0f0f0",
+    },
+  },
+  typography: {
+    fontFamily: "Noto Sans JP, sans-serif",
+    h1: {
+      fontWeight: "bold",
+    },
+    h2: {
+      fontWeight: "bold",
+      fontSize: "2rem",
+      borderBottom: "1px solid",
+      borderTop: "1px solid",
+      borderColor: "rgb(0, 160, 125)",
+      paddingBottom: "0.5rem",
+      marginBottom: "1rem",
+      paddingTop: "0.5rem",
+      marginTop: "1rem",
+      color: "rgb(0, 160, 125)",
+    },
+  },
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: { variant: "code" },
+          style: {
+            fontFamily: "Noto Sans JP, sans-serif",
+            fontStyle: "italic",
+            fontSize: "0.8rem",
+            backgroundColor: "rgb(240, 240, 240)",
+            padding: "0.2rem",
+            margin: "0.2rem",
+          },
+        },
+      ],
     },
   },
 });
