e8449ec03f82404a475110a4a60c998b22635c65
KatsuhiroNakai <k.nakai.research@gmail.com>
2025-05-24 12:14:11 +0900

    feat: Baseコンポーネントにフロントエンド開発に必要な基本的なCSSやHTMLの内容を説明するテキストを追加。これにより、開発者が使用するTailwindやMUIのバージョンに関する情報を明確にし、理解を助ける。

--- src/components/templates/Base/index.tsx
+++ src/components/templates/Base/index.tsx
@@ -5,6 +5,8 @@ const Base = () => {
   return (
     <Box>
       <Typography>
+        ここでは基本的なCSSやHTMLの内容，レイアウト要素など，フロントエンド開発に必要不可欠な要素を扱う．
+        <br />
         使用するTailwindのバージョンやMUIのバージョンは
         <Link text="ここ" url="/" />
         を参照してください
