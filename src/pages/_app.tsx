29a435c2dd59d085a22f0a864f2e12906b6527c5
KatsuhiroNakai <k.nakai.research@gmail.com>
2025-05-24 22:31:27 +0900

    feat: ページタイトルを「デザインガイド」に設定し、Headコンポーネントを追加。これにより、SEO対策とユーザー体験が向上。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,20 @@ import { ThemeProvider } from "@mui/material/styles";
 import type { AppProps } from "next/app";
 import SideMenu from "@/features/SideMenu";
 import { Box } from "@mui/material";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ display: "flex" }}>
-        <SideMenu />
-        <Component {...pageProps} />
-      </Box>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>デザインガイド</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: "flex" }}>
+          <SideMenu />
+          <Component {...pageProps} />
+        </Box>
+      </ThemeProvider>
+    </>
   );
 }
